a2f7909af8c3bafd0d8eca2261e78fa2e9af3b89
ViktorMarhitich <victormargitich@gmail.com>
2021-07-15 09:58:02 +0300

    132

--- Model/MasterContext.cs
+++ Model/MasterContext.cs
@@ -28,7 +28,7 @@ namespace TimeOffTracker.Model
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost; user=SA; password=456789Zxc");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-VQDACQU; user=sa password=111");
             }
         }
 
