d59322d432237790bc32a5c1b87ac0b10f69e093
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2021-07-22 01:47:11 +0300

    Добавил front для авторизации
    Сейчас авторизация условная - просто проверка, если ли jwt token в куки. Проверка пользователя в разработке.
    Добавил перенаправление при заходе неавторизованного пользователя. Добавил отправку запроса с формы, добавил установку jwt токена в куки. Почистил лишние вьюшки в ротуере.
    
    Код в состоянии доработки, но уже рабочий.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import { Authorization } from "./components/Authorization";
+import React, {Component} from 'react';
+import {Route} from "react-router-dom";
+import {Redirect} from 'react-router'
+import {Layout} from './components/Layout';
+import {Home} from './components/Home';
+import {Authorization} from "./components/Authorization";
+import {AuthService} from "./Services/AuthService";
 
 import './custom.css'
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
+    
+    constructor(props) {
+        super(props);
+    }
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/auth' component={Authorization} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route exact path="/" component={Home}>
+                    {!AuthService.isLogged() ? <Redirect to="/auth">Protected Page</Redirect> : <Home/>}
+                </Route>
+                <Route path='/auth' component={Authorization}/>
+            </Layout>
+        );
+    }
 } 
---
+++ ClientApp/src/Services/AuthService.js
@@ -0,0 +1,16 @@
+﻿import Cookies from 'js-cookie'
+import jwt_decode from "jwt-decode"
+
+export class AuthService {
+    static isLogged() {
+        return Cookies.get("token") !== undefined;
+    }
+
+    static logOut() {
+        try {
+            Cookies.remove('token');
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
