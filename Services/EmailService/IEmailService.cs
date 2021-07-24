5f3b097996d80f9ed86361a44222064b1f7ba749
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2021-07-25 02:06:52 +0300

    add EmailService

---
+++ Services/EmailService/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace TimeOffTracker.Services.EmailService {
+    public interface IEmailService {
+        public Task SendEmail(MailAddress to, string subject, string html);
+    }
+}
