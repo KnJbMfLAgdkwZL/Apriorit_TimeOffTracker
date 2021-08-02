8be8dc4f35fdeb3cca745339d2e6bd83cfe830d9
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2021-08-02 15:15:57 +0300

    Create DateFormatter.js

---
+++ ClientApp/src/Services/DateFormatter.js
@@ -0,0 +1,15 @@
+﻿export class DateFormatter {
+    // Format is "yyyy-mm-dd"
+    static dateToString(date: Date): string {
+        let dd, mm, yyyy = "";
+        if (date.getDate() < 10) {
+            dd = "0" + date.getDate();
+        }
+        if (date.getMonth() + 1 < 10) {
+            mm = "0" + (date.getMonth() + 1);
+        }
+        yyyy = date.getFullYear();
+        
+        return yyyy + "-" + mm + "-" + dd;
+    }
+}
