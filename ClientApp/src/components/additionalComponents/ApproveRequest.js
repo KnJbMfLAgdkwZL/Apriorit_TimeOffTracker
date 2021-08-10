367d2771c827f7913e7f8e1b6f6894fb8bd031b5
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2021-08-10 21:42:54 +0300

    approve and reject request pages
    should be tested

---
+++ ClientApp/src/components/additionalComponents/ApproveRequest.js
@@ -0,0 +1,85 @@
+﻿import React, { Component } from 'react';
+import {RequestSendingService} from "../../Services/RequestSendingService";
+import {URL} from "../../GlobalSettings/URL";
+import {getSearchParams} from "../../Helpers/UrlParametersParser";
+import {Col, Container, Row} from "reactstrap";
+
+export class ApproveRequest extends Component {
+    static displayName = ApproveRequest.name;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: false,
+            errorValue: "",
+            loading: false,
+            ok: false,
+        }
+    }
+    
+    async componentDidMount(): Promise<void> {
+        this.setState({
+            loading: true,
+        });
+
+        await RequestSendingService.sendGetRequestAuthorized(URL.url + "Manager/AcceptRequest?id=" + getSearchParams("id"))
+            .then(async res => {
+                if (res.status === 200) {
+                    this.setState({
+                        loading: false,
+                        error: false,
+                        ok: true
+                    })
+                }
+                else if (res.status === 500) {
+                    this.setState({
+                        loading: false,
+                        error: true,
+                        errorValue: "Server error...",
+                    })
+                }
+                else {
+                    const d = await res.json().then(d => d);
+                    console.log(d);
+                    this.setState({
+                        loading: false,
+                        error: true,
+                        errorValue: (d.title === undefined) ? d : d.title,
+                    })
+                }
+            })
+            .catch(e => {
+                this.setState({
+                    loading: false,
+                    error: true,
+                })
+                console.error(e);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <center><p><strong>
+                                Approve Page
+                            </strong></p></center>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <center><p><strong>
+                                Result:
+                                {!this.state.error && this.state.ok && <font color="green"> Request has been sent! </font>}
+                                {this.state.error && <font color="red"> {this.state.errorValue} </font>}
+                            </strong></p></center>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
---
+++ ClientApp/src/components/additionalComponents/RejectRequest.js
@@ -0,0 +1,121 @@
+﻿import React, { Component } from 'react';
+import TextField from "@material-ui/core/TextField";
+import {RequestSendingService} from "../../Services/RequestSendingService";
+import {URL} from "../../GlobalSettings/URL";
+import {getSearchParams} from "../../Helpers/UrlParametersParser";
+import {Button, Col, Container, Row} from "reactstrap";
+
+export class RejectRequest extends Component {
+    static displayName = RejectRequest.name;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            textFieldReasonValue: "",
+            error: false,
+            errorValue: "",
+            loading: false,
+            ok: false,
+        }
+
+        this.sendRejectRequest = this.sendRejectRequest.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <center><p><strong>
+                                Approve Page
+                            </strong></p></center>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <center><p><strong>
+                                Result:
+                                {!this.state.error && this.state.ok && <font color="green"> Request has been sent! </font>}
+                                {this.state.error && <font color="red"> {this.state.errorValue} </font>}
+                            </strong></p></center>
+                        </Col>
+                    </Row>                    
+                    <Row>
+                        <Col>
+                            <TextField
+                                value={this.state.textFieldReasonValue}
+                                onChange={this._handleTextFiledReasonChange}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="Reason"
+                                name="Reason"
+                                label="Reason"
+                                helperText="Specify the reason of rejection"
+                                error={this.state.error}
+                            />
+                        </Col>
+                        <Col>
+                            <Button outline
+                                    onClick={this.sendRejectRequest}
+                                    block
+                                    color="danger">
+                                To Reject Page
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+
+    _handleTextFiledReasonChange(e) {
+        this.setState({
+            textFieldReasonValue: e.target.value
+        });
+    }
+
+    async sendRejectRequest() {
+        this.setState({
+            loading: true,
+        });
+
+        await RequestSendingService.sendPostRequestAuthorized(URL.url + "Manager/RejectRequest?id=" + getSearchParams("id"), {
+            reason: this.state.textFieldReasonValue
+        })
+            .then(async res => {
+                if (res.status === 200) {
+                    this.setState({
+                        loading: false,
+                        error: false,
+                        ok: true
+                    })
+                }
+                else if (res.status === 500) {
+                    this.setState({
+                        loading: false,
+                        error: true,
+                        errorValue: "Server error...",
+                    })
+                }
+                else {
+                    const d = await res.json().then(d => d);
+                    console.log(d);
+                    this.setState({
+                        loading: false,
+                        error: true,
+                        errorValue: (d.title === undefined) ? d : d.title,
+                    })
+                }
+            })
+            .catch(e => {
+                this.setState({
+                    loading: false,
+                    error: true,
+                })
+                console.error(e);
+            })
+    }
+}
